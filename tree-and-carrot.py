6a8e9ca511f511f128e36ec34c89ace14210bd43
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-22 21:50:04 +0200

    refactor: simplify column movement logic

--- tree-and-carrot.py
+++ tree-and-carrot.py
@@ -1,13 +1,11 @@
 clear()
 
-first_run = True
-
 def get_boolean_number(is_even):
     if is_even:
         return 1
     return 0
 
-def column_movement(is_even):
+def column_movement(is_even, should_till):
     number = get_boolean_number(is_even)
 
     for i in range(get_world_size()):
@@ -16,33 +14,22 @@ def column_movement(is_even):
         if get_pos_y() % 2 == number:
             plant(Entities.Tree)
         else:
-            till()
+            if should_till:
+                till()
             plant(Entities.Carrot)
 
         move(North)
 
-def column_movement_no_till(is_even):
-    number = get_boolean_number(is_even)
+first_run = True
+counter = 0
 
-    for i in range(get_world_size()):
-        if can_harvest():
-            harvest()
-        if get_pos_y() % 2 == number:
-            plant(Entities.Tree)
-        else:
-            plant(Entities.Carrot)
+while True:
+    is_column_even = get_pos_x() % 2 == 0
 
-        move(North)
-
-while first_run:
-    for i in range(get_world_size()):
-        is_even = get_pos_x() % 2 == 0
-        column_movement(is_even)
-        move(East)
-    
-    first_run = False
-
-while not first_run:
-    is_even = get_pos_x() % 2 == 0
-    column_movement_no_till(is_even)
+    column_movement(is_column_even, first_run)
     move(East)
+
+    if first_run:
+        counter += 1
+        if get_world_size() == counter:
+            first_run = False
