2df3b985ab48eb906cc33bb2419eb7bd5d0cf0d6
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-23 18:55:50 +0200

    feat: add move to the middle function

--- one_to_rule_them_all.py
+++ one_to_rule_them_all.py
@@ -8,3 +8,10 @@ def plant_smth(entity, entity_ground_type, water):
 def move_and_plant(entity, ground, direction, water):
 	move(direction)
 	plant_smth(entity, ground, water)
+
+# This function will work if starting position is 0,0
+def move_to_the_middle():
+	half_size = (get_world_size() / 2) - 1
+	for i in range(half_size):
+		move(North)
+		move(East)
