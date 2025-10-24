781f121ec363dd68216198697fd1ca2b51b3f2ca
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-24 08:55:34 +0200

    feat: new carrot farming script
    backup old script

--- carrot.py
+++ carrot.py
@@ -1,27 +1,71 @@
+import one_to_rule_them_all
+
 clear()
 
-first_run = True
-cols_completed = 0
+entity = Entities.Carrot
+ground = Grounds.Soil
+shouldWater = False
+
+move_north = True
+move_east = True
+
+farm_limit = 6
+max_farm = False
 world_size = get_world_size()
 
-while first_run:
-	for j in range(get_world_size()):
-		harvest()
-		till()
-		plant(Entities.Carrot)
-		move(North)
-	move(East)
-	cols_completed += 1
-	if world_size == cols_completed:
-		break
-	
-first_run = False
+if farm_limit > world_size:
+	farm_limit = world_size
 
-while not first_run:
-	for i in range(get_world_size()):
-		while not can_harvest():
-			do_a_flip()	
-		harvest()
-		plant(Entities.Carrot)
-		move(North)
-	move(East)
+if farm_limit == world_size:
+	max_farm = True
+
+while True:
+	# moving over Y axis
+	for i in range(farm_limit):
+		# moving over X axis
+		for j in range(farm_limit):
+			if can_harvest():
+				harvest()
+			one_to_rule_them_all.plant_smth(entity, ground, shouldWater)
+
+			if j != farm_limit - 1:
+				if move_east:
+					move(East)
+				else:
+					move(West)
+		if i != farm_limit - 1:
+			if move_north:
+				move(North)
+			else:
+				move(South)
+
+		# go back to the row start
+
+		# when farm is max size go
+		# over the edge
+		if max_farm:
+			if move_east:
+				move(East)
+			else:
+				move(West)
+		# when farm is smaller, go
+		# back using the opposite
+		# direction
+		else:
+			for i in range(farm_limit - 1):
+				if not move_east:
+					move(East)
+				else:
+					move(West)
+
+	if max_farm:
+		if move_north:
+			move(North)
+		else:
+			move(South)
+	else:
+		for i in range(farm_limit - 1):
+			if not move_north:
+				move(North)
+			else:
+				move(South)
