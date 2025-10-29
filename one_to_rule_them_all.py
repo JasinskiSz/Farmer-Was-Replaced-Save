3e03254a78cb539f5ab9918612ac17fc62815108
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-29 11:10:29 +0100

    feat: water until watered

--- one_to_rule_them_all.py
+++ one_to_rule_them_all.py
@@ -2,8 +2,9 @@ def plant_smth(entity, entity_ground_type, water):
 	if get_ground_type() != entity_ground_type:
 		till()
 	plant(entity)
-	if water and get_water() < 0.75:
-		use_item(Items.Water)
+	if water:
+		while get_water() < 0.75:
+			use_item(Items.Water)
 
 def move_and_plant(entity, ground, direction, water):
 	move(direction)
