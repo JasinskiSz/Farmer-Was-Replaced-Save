c2b5d7895d23f4280dec8ae007bb8933375ace8d
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-29 14:21:54 +0100

    feat: rework the carrot script

--- carrot.py
+++ carrot.py
@@ -2,70 +2,13 @@ import one_to_rule_them_all
 
 clear()
 
+x_size = 6
+y_size = 6
 entity = Entities.Carrot
 ground = Grounds.Soil
-shouldWater = False
-
+loop = True
+use_water = True
 move_north = True
 move_east = True
 
-farm_limit = 6
-max_farm = False
-world_size = get_world_size()
-
-if farm_limit > world_size:
-	farm_limit = world_size
-
-if farm_limit == world_size:
-	max_farm = True
-
-while True:
-	# moving over Y axis
-	for i in range(farm_limit):
-		# moving over X axis
-		for j in range(farm_limit):
-			if can_harvest():
-				harvest()
-			one_to_rule_them_all.plant_smth(entity, ground, shouldWater)
-
-			if j != farm_limit - 1:
-				if move_east:
-					move(East)
-				else:
-					move(West)
-		if i != farm_limit - 1:
-			if move_north:
-				move(North)
-			else:
-				move(South)
-
-		# go back to the row start
-
-		# when farm is max size go
-		# over the edge
-		if max_farm:
-			if move_east:
-				move(East)
-			else:
-				move(West)
-		# when farm is smaller, go
-		# back using the opposite
-		# direction
-		else:
-			for i in range(farm_limit - 1):
-				if not move_east:
-					move(East)
-				else:
-					move(West)
-
-	if max_farm:
-		if move_north:
-			move(North)
-		else:
-			move(South)
-	else:
-		for i in range(farm_limit - 1):
-			if not move_north:
-				move(North)
-			else:
-				move(South)
+one_to_rule_them_all.farm(x_size, y_size, entity, ground, loop, use_water, move_north, move_east)
